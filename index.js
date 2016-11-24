6abb179c5f218ef79cacff7db6f0d48c2367a421
jurien <jurienhamaker@gmail.com>
2016-11-24 16:18:32 +0100

    customizable options

--- index.js
+++ index.js
@@ -12,30 +12,51 @@ var urlencode = require('./lib/urlencode');
 var createOAuthHeader = require('./lib/createOAuthHeader'),
 	checkOptions = require('./lib/checkOptions');
 
-// constructor/class
+/**
+ * CMPay class
+ * Everything public which could be needed
+ */
 class CMPay {
+	/**
+	 * Constructor for CMPay, sets the options.
+	 * @param  {[object]}  Object with options.
+	 */
 	constructor(options){
 		this.options = checkOptions(options);
+
+		if(this.options.debug) {
+			require('request-debug')(request);
+		}
 	}
 	
+	/**
+	 * Get the signing key based off of the consumerKey & secretKey
+	 * @return {[string]}
+	 */
 	getSigningKey() {
 		return urlencode(this.options.consumerKey) + '&' + urlencode(this.options.secretKey);
 	}
 
-	sendRequest(method, url, body) {
+	/**
+	 * @param {string} url the url to call to
+	 * @param {object} body body data if needed
+	 * @param {string} method the method to be used ['get', 'post', 'put', 'delete']
+	 * @return {Promise<object>} CMPay response
+	 */
+	sendRequest(url, body, method = 'GET') {
 		return new Promise((resolve, reject) => {
 			let fullUrl = this.options.apiUrl + url;
 
 			let httpOptions = {
 				url: fullUrl,
-				method: method.toUpperCase(),
-				json: true
+				method: method.toUpperCase()
 			};
 
 			if(body && typeof body === 'object') {
-				httpOptions.body = body;
+				httpOptions.body = JSON.stringify(body);
 			}
 
+			// create the oAuth header needed for the Authorization header.
 			createOAuthHeader(method, fullUrl, body, this.options.consumerKey, this.getSigningKey())
 			.then(result => {
 				httpOptions.headers = {
@@ -45,13 +66,20 @@ class CMPay {
 
 				request(httpOptions, (err, res, resBody) => {
 					if(err) { return reject(err); }
-					resolve(resBody);
+					resolve(JSON.parse(resBody));
 				});
 			}, err => reject(err));
 		})
 	}
 
-	createPaymentDetails(issuerId, purchaseId, description) {
+	/**
+	 * @param {string} issuerId ID of the issuer (bank)	
+	 * @param {string} purchageId identifier to recognize your payment
+	 * @param {string} description Optional description for the payment
+	 * @param {object} returnUrls object with return urls if you want to use something else than the config defined
+	 * @return {object} CMPay formatted payment details
+	 */
+	createPaymentDetails(issuerId, purchaseId, description, returnUrls = false) {
 		if(!issuerId) {
 			throw new Error('issuerId is a required variable.');
 		}
@@ -61,57 +89,79 @@ class CMPay {
 		}
 
 		return {
-			issuer_id: issuerId,
-			success_url: this.options.returnUrls.success,
-			cancelled_url: this.options.returnUrls.cancel,
-			failed_url: this.options.returnUrls.fail,
-			expired_url: this.options.returnUrls.error,
-			purchase_id: purchaseId,
-			description: description ? description : ''
+			"issuer_id": issuerId,
+			"success_url": (returnUrls && returnUrls.success) ? returnUrls.success : this.options.returnUrls.success,
+			"cancelled_url": (returnUrls && returnUrls.cancel) ? returnUrls.cancel : this.options.returnUrls.cancel,
+			"failed_url": (returnUrls && returnUrls.fail) ? returnUrls.fail : this.options.returnUrls.fail,
+			"expired_url": (returnUrls && returnUrls.error) ? returnUrls.error : this.options.returnUrls.error,
+			"purchase_id": purchaseId,
+			"description": description ? description : ''
 		};
 	}
 
-	createIdealPayment(amount, issuerId, purchaseId, description) {
+	/**
+	 * @param {double} amount The amount to be paid
+	 * @param {string} issuerId ID of the issuer (bank)	
+	 * @param {string} purchageId identifier to recognize your payment
+	 * @param {string} description Optional description for the payment
+	 * @return {object} CMPay formatted payment
+	 */
+	createIdealPayment(amount, issuerId, purchaseId, description, returnUrls = false) {
 		return {
-			amount: amount,
-			currency: this.options.currency,
-			payment_method: 'iDEAL',
-			payment_details: this.createPaymentDetails(issuerId, purchaseId, description)
+			"amount": amount,
+			"currency": this.options.currency,
+			"payment_method": "iDEAL",
+			"payment_details": this.createPaymentDetails(issuerId, purchaseId, description, returnUrls)
 		};
 	}
 
+	/**
+	 * @return {Promise} request promise object.
+	 */
 	getBankList() {
-		return this.sendRequest('get', '/issuers/v1/ideal');
+		return this.sendRequest('/issuers/v1/ideal');
 	}
 
+	/**
+	 * @param  {object} payment A formatted payment object (create payment functions)
+	 * @return {Promise} request promise object.
+	 */
 	createCharge(payment) {
 		if(typeof payment !== 'object') {
 			throw new Error('Payments must be an object, use one of the createPayments methods to create a payment.');
 		}
 
 		let data = {
-			amount: payment.amount,
-			currency: this.options.currency,
-			payments: [payment]
+			"amount": payment.amount,
+			"currency": this.options.currency,
+			"payments": [payment]
 		};
 
-		return this.sendRequest('post', '/charges/v1', data);
+		return this.sendRequest('/charges/v1', data, 'post');
 	}
 
+	/**
+	 * @param  {string} chargeId the id of a charge
+	 * @return {Promise} request promise object.
+	 */
 	getCharge(chargeId) {
 		if(!chargeId) {
 			throw new Error('ChargeId is a required variable.');
 		}
 
-		return this.sendRequest('get', '/charges/v1/' + chargeId);
+		return this.sendRequest('/charges/v1/' + chargeId);
 	}
 
+	/**
+	 * @param  {string} paymentId the id of a payment
+	 * @return {Promise} request promise object.
+	 */
 	getPayment(paymentId) {
 		if(!paymentId) {
 			throw new Error('paymentId is a required variable.');
 		}
 
-		return this.sendRequest('get', '/payments/v1/' + paymentId);
+		return this.sendRequest('/payments/v1/' + paymentId);
 	}
 }
 
